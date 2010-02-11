eef126edc322818790a62a3b116cf04d541ee760
Vasco Fernandes <vasco.box@gmail.com>
2010-02-11 19:11:50 +0000

    chord skeleton

---
+++ simple_chord_example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from micropeers import reactor, communication, webserver
+
+class Chord(BasePeer):
+    id = None
+    storage = {}
+
+    def run(own_id, connection_id):
+        pass
+
+    def lookup(key):
+        pass
+
+    def store(key, value):
+        pass
+
+    def new_node(id):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__'
+    main()
